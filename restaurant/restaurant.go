3ecd741275c3a35c9c3e1f06b1c7d8d6963776ec
Dan Chen <dan.chen@boardware.com>
2023-11-20 17:51:26 +0800

    feat

--- restaurant/restaurant.go
+++ restaurant/restaurant.go
@@ -63,10 +63,6 @@ func (r Restaurant) AddPrinter(printer *Printer) *Printer {
 	return printer
 }
 
-func (r Restaurant) Printers() []Printer {
-	return restaurantRepository.printerRepository.List("restaurant_id = ?", r.ID())
-}
-
 func (r Restaurant) PickUpCode() int64 {
 	var bill Bill
 	billRepository.db.Order("pick_up_code DESC").Find(&bill, "restaurant_id = ?", r.ID())
