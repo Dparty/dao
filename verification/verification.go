768f1fff262f5bea1ffff686d34de86d97b3c054
Dan Chen <dan.chen@boardware.com>
2023-11-30 15:59:46 +0800

    feat

--- verification/verification.go
+++ verification/verification.go
@@ -8,4 +8,5 @@ type VerificationCode struct {
 	AreaCode    *string `json:"areaCode" gorm:"type:CHAR(8);index:verification_area_code_index"`
 	PhoneNumber *string `json:"phonenumber" gorm:"index:verification_phone_number_index"`
 	Code        string  `json:"code" gorm:"type:VARCHAR(12)"`
+	Purpose     *string `json:"purpost" gorm:"type:VARCHAR(12)"`
 }
